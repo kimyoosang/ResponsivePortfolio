0adfd0d40d0acc186c44084e1eb8a9b22c08dad4
kimyoosang <yoo99485@gmail.com>
2022-01-27 02:37:50 +0900

    [Add] AboutMe component

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./AboutMe.css";
+
+function AboutMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fedeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const SCREEN_CONSTSANTS = {
+    description:
+      "리액트와 javascript,Redux,Hooks를 사용하는 프론트엔드 개발자입니다. 학부시절 C언어와 Python을 배운 경험이 있고, 코드스테이츠라는 부트캠프에서 Javascript, Node Js로 풀스택 엔지니어링 과정을 수료하였습니다",
+    highlights: {
+      bullets: [
+        "프론트엔드 개발자",
+        "반응형 웹 개발과 UI 퍼블리싱",
+        "React",
+        "REST API",
+        "협업에 강점",
+      ],
+      heading: "Here are a Few Highlights",
+    },
+  };
+
+  const renderHighlight = () => {
+    return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div
+      className="about-me-container screen-container fade-in"
+      id={props.id || ""}
+    >
+      <div className="about-me-parent">
+        <ScreenHeading title={"About ME"} subHeading={"Why Choose Me"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTSANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlights-heading">
+                <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {" "}
+                Hire Me{" "}
+              </button>
+              <a
+                href="프론트엔드 김유상.pdf"
+                download={"프론트엔드 김유상.pdf"}
+              >
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
