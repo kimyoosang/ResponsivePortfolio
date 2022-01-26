104b7bed99c76450fd568649650077dc600624ce
kimyoosang <yoo99485@gmail.com>
2022-01-27 01:09:01 +0900

    [ADD] Footer component

---
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import shape from "../../../assets/Home/shape-bg.png";
+
+function Footer() {
+  return (
+    <div className="footler-container">
+      <div className="footer-parent">
+        <img src={shape} alt="you have problem with the image" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
