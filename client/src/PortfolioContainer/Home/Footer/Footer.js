145d8b4b514b3c7597d752b7c7a648b82b3151a4
kimyoosang <yoo99485@gmail.com>
2022-01-27 01:14:54 +0900

    [Update] Footer responsive

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Footer.css";
 import shape from "../../../assets/Home/shape-bg.png";
 
 function Footer() {
   return (
-    <div className="footler-container">
+    <div className="footer-container">
       <div className="footer-parent">
         <img src={shape} alt="you have problem with the image" />
       </div>
